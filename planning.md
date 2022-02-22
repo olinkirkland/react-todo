7d7f9e646f438d0fc4ba22beae69b575b3d321e1
Olin Kirkland <me@olinkirk.land>
2022-02-22 17:16:56 +0100

    Addt. bootstrap

---
+++ planning.md
@@ -0,0 +1,12 @@
+### Components
+1. Creating todos (input field, button)
+2. Liste mit items
+3. Item
+
+### Item Actions
+1. Add
+2. Edit
+3. Mark as Done
+4. Delete
+
+?  Edit (Modal)
--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
-      <div className="container">
+      <div className="container border bg-light m-4">
         <h1 class="display-1">Hallo, Welt!</h1>
+        <input></input>
         <button class="btn btn-success">Click here!</button>
       </div>
     </>
