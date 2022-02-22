365ba931a50360d8debd736931e67b74e80ea663
Olin Kirkland <me@olinkirk.land>
2022-02-22 17:29:46 +0100

    Added some bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
-      <div className="container border bg-light m-4">
-        <h1 class="display-1">Hallo, Welt!</h1>
-        <input></input>
-        <button class="btn btn-success">Click here!</button>
+      <div className="container border bg-light p-4 m-4">
+        {/* <h1 class="display-1">Hallo, Welt!</h1> */}
+        <form>
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Enter text here"
+            />
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" type="button">
+                Add
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </>
   );
