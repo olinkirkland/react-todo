988bf26fa598e434c08e89da0f0f5d2b30b977f7
Olin Kirkland <me@olinkirk.land>
2022-02-22 16:49:47 +0100

    Setup bootstrap/jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 class="display-1">Hallo, Welt!</h1>
+        <button class="btn btn-success">Click here!</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
